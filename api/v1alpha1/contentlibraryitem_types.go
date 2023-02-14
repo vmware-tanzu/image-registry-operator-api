93e282d016dada758bc267de2a6158ea16bfaf36
Sreeja Acharya <acharyasreej@vmware.com>
2023-02-13 22:33:08 -0800

    Addressed review comments

--- api/v1alpha1/contentlibraryitem_types.go
+++ api/v1alpha1/contentlibraryitem_types.go
@@ -1,10 +1,12 @@
-// Copyright (c) 2022 VMware, Inc. All Rights Reserved.
+// Copyright (c) 2022-2023 VMware, Inc. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 package v1alpha1
 
 import (
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 // ContentLibraryItemType is a constant for the type of a content library item in vCenter.
@@ -15,44 +17,35 @@ type CertVerificationStatus string
 
 const (
 	// ContentLibraryItemTypeOvf indicates an OVF content library item in vCenter.
-	ContentLibraryItemTypeOvf = ContentLibraryItemType("Ovf")
+	ContentLibraryItemTypeOvf ContentLibraryItemType = "OVF"
 
 	// ContentLibraryItemTypeIso indicates an ISO content library item in vCenter.
-	ContentLibraryItemTypeIso = ContentLibraryItemType("Iso")
+	ContentLibraryItemTypeIso ContentLibraryItemType = "ISO"
 
 	// CertVerificationStatusNotAvailable indicates the certificate verification status is not available.
-	CertVerificationStatusNotAvailable = CertVerificationStatus("NOT_AVAILABLE")
+	CertVerificationStatusNotAvailable CertVerificationStatus = "NOT_AVAILABLE"
 
 	// CertVerificationStatusVerified indicates the library item has been fully validated during importing or file syncing.
-	CertVerificationStatusVerified = CertVerificationStatus("VERIFIED")
+	CertVerificationStatusVerified CertVerificationStatus = "VERIFIED"
 
 	// CertVerificationStatusInternal indicates the library item is cloned/created through vCenter.
-	CertVerificationStatusInternal = CertVerificationStatus("INTERNAL")
+	CertVerificationStatusInternal CertVerificationStatus = "INTERNAL"
 
 	// CertVerificationStatusVerificationFailure indicates certificate or manifest validation failed on the library item.
-	CertVerificationStatusVerificationFailure = CertVerificationStatus("VERIFICATION_FAILURE")
+	CertVerificationStatusVerificationFailure CertVerificationStatus = "VERIFICATION_FAILURE"
 
 	// CertVerificationStatusVerificationInProgress indicates the library item certificate verification is in progress.
-	CertVerificationStatusVerificationInProgress = CertVerificationStatus("VERIFICATION_IN_PROGRESS")
+	CertVerificationStatusVerificationInProgress CertVerificationStatus = "VERIFICATION_IN_PROGRESS"
 
 	// CertVerificationStatusUntrusted indicates the certificate used to sign the library item is not trusted.
-	CertVerificationStatusUntrusted = CertVerificationStatus("UNTRUSTED")
+	CertVerificationStatusUntrusted CertVerificationStatus = "UNTRUSTED"
 )
-
-// ContentLibraryReference contains the information to locate the content library resource.
-type ContentLibraryReference struct {
-	// Name is the name of resource being referenced.
-	// +required
-	Name string `json:"name"`
-
-	// Namespace of the resource being referenced. If empty, cluster scoped resource is assumed.
-	// +optional
-	Namespace string `json:"namespace,omitempty"`
-}
 
 type CertificateVerificationInfo struct {
 	// Status shows the certificate verification status of the library item.
-	Status CertVerificationStatus `json:"status"`
+	// +kubebuilder:validation:Enum=NOT_AVAILABLE;VERIFIED;INTERNAL;VERIFICATION_FAILURE;VERIFICATION_IN_PROGRESS;UNTRUSTED
+	// +optional
+	Status CertVerificationStatus `json:"status,omitempty"`
 
 	// CertChain shows the signing certificate in base64 encoding if the library item is signed.
 	// +optional
@@ -63,46 +56,46 @@ type CertificateVerificationInfo struct {
 type ContentLibraryItemSpec struct {
 	// UUID is the identifier which uniquely identifies the library item in vCenter. This field is immutable.
 	// +required
-	UUID string `json:"uuid"`
+	UUID types.UID `json:"uuid"`
 }
 
 // ContentLibraryItemStatus defines the observed state of ContentLibraryItem.
 type ContentLibraryItemStatus struct {
 	// Name specifies the name of the content library item in vCenter specified by the user.
-	// +required
-	Name string `json:"name"`
+	// +optional
+	Name string `json:"name,omitempty"`
 
 	// ContentLibraryRef refers to the ContentLibrary custom resource that this item belongs to.
-	// +required
-	ContentLibraryRef ContentLibraryReference `json:"contentLibraryRef"`
+	// +optional
+	ContentLibraryRef *NameAndKindRef `json:"contentLibraryRef,omitempty"`
 
 	// Description is a human-readable description for this library item.
 	// +optional
 	Description string `json:"description,omitempty"`
 
-	// MetadataVersion indicates the version of the library item metadata.
+	// MetadataVersion indicates the version of the library item metadata in vCenter.
 	// This integer value is incremented when the library item properties such as name or description are changed in vCenter.
-	// +required
-	MetadataVersion string `json:"metadataVersion"`
+	// +optional
+	MetadataVersion string `json:"metadataVersion,omitempty"`
 
-	// ContentVersion indicates the version of the library item content.
+	// ContentVersion indicates the version of the library item content in vCenter.
 	// This integer value is incremented when the files comprising the content library item are changed in vCenter.
-	// +required
-	ContentVersion string `json:"contentVersion"`
+	// +optional
+	ContentVersion string `json:"contentVersion,omitempty"`
 
 	// Type string indicates the type of the library item in vCenter.
-	// Possible types are "Ovf" and "Iso".
-	// +required
-	Type ContentLibraryItemType `json:"type"`
-
-	// Size indicates the library item size in bytes
+	// +kubebuilder:validation:Enum=OVF;ISO
 	// +optional
-	Size int32 `json:"size,omitempty"`
+	Type ContentLibraryItemType `json:"type,omitempty"`
+
+	// Size indicates the library item size in bytes in vCenter.
+	// +optional
+	Size resource.Quantity `json:"size,omitempty"`
 
 	// Cached indicates if the library item files are on disk in vCenter.
-	// +required
+	// +optional
 	// +kubebuilder:default=false
-	Cached bool `json:"cached"`
+	Cached bool `json:"cached,omitempty"`
 
 	// SecurityCompliance shows the security compliance of the library item.
 	// +optional
@@ -113,23 +106,28 @@ type ContentLibraryItemStatus struct {
 	CertificateVerificationInfo *CertificateVerificationInfo `json:"certificateVerificationInfo,omitempty"`
 
 	// Ready denotes that the library item is ready to be used.
-	// +required
-	// +kubebuilder:default=false
-	Ready bool `json:"ready"`
-
-	// CreationTime indicates the date and time when this library item was created.
-	// +required
-	CreationTime string `json:"creationTime"`
-
-	// LastModifiedTime indicates the date and time when this library item was last updated.
-	// This field is updated when the library item properties are changed or the file content is changed.
-	// +required
-	LastModifiedTime string `json:"lastModifiedTime"`
-
-	// LastSyncTime indicates the date and time when this library item was last synchronized.
-	// This field applies only to subscribed library items.
+	// This flag is set only after all the files associated with the content library item have been uploaded to vCenter.
 	// +optional
-	LastSyncTime string `json:"lastSyncTime,omitempty"`
+	// +kubebuilder:default=false
+	Ready bool `json:"ready,omitempty"`
+
+	// Files represent zero, one or more files belonging to the content library item in vCenter.
+	// +optional
+	// Files []string `json:"files,omitempty"`
+
+	// CreationTime indicates the date and time when this library item was created in vCenter.
+	// +optional
+	CreationTime metav1.Time `json:"creationTime,omitempty"`
+
+	// LastModifiedTime indicates the date and time when this library item was last updated in vCenter.
+	// This field is updated when the library item properties are changed or the file content is changed.
+	// +optional
+	LastModifiedTime metav1.Time `json:"lastModifiedTime,omitempty"`
+
+	// LastSyncTime indicates the date and time when this library item was last synchronized in vCenter.
+	// This field applies only to the library items belonging to the library of Type=Subscribed.
+	// +optional
+	LastSyncTime metav1.Time `json:"lastSyncTime,omitempty"`
 
 	// Conditions describes the current condition information of the ContentLibraryItem.
 	// +optional
@@ -171,6 +169,46 @@ type ContentLibraryItemList struct {
 	Items           []ContentLibraryItem `json:"items"`
 }
 
+func (cclItem *ClusterContentLibraryItem) GetConditions() Conditions {
+	return cclItem.Status.Conditions
+}
+
+func (cclItem *ClusterContentLibraryItem) SetConditions(conditions Conditions) {
+	cclItem.Status.Conditions = conditions
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:scope=Cluster,shortName=cclitem
+// +kubebuilder:printcolumn:name="vSphereName",type="string",JSONPath=".status.name"
+// +kubebuilder:printcolumn:name="ClusterContentLibraryRef",type="string",JSONPath=".status.clusterContentLibraryRef"
+// +kubebuilder:printcolumn:name="Type",type="string",JSONPath=".status.type"
+// +kubebuilder:printcolumn:name="Ready",type="boolean",JSONPath=".status.ready"
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
+
+// ClusterContentLibraryItem is the schema for the content library item API at the cluster scope.
+// Currently, ClusterContentLibraryItem are immutable to end users.
+type ClusterContentLibraryItem struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ContentLibraryItemSpec   `json:"spec,omitempty"`
+	Status ContentLibraryItemStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// ClusterContentLibraryItemList contains a list of ClusterContentLibraryItem.
+type ClusterContentLibraryItemList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ClusterContentLibraryItem `json:"items"`
+}
+
 func init() {
-	RegisterTypeWithScheme(&ContentLibraryItem{}, &ContentLibraryItemList{})
+	RegisterTypeWithScheme(
+		&ContentLibraryItem{},
+		&ContentLibraryItemList{},
+		&ClusterContentLibraryItem{},
+		&ClusterContentLibraryItemList{})
 }
--- hack/tools/go.mod
+++ hack/tools/go.mod
@@ -4,7 +4,7 @@ go 1.18
 
 require (
 	github.com/golangci/golangci-lint v1.50.0
-	sigs.k8s.io/controller-tools v0.9.2
+	sigs.k8s.io/controller-tools v0.10.0
 )
 
 require (
@@ -43,8 +43,7 @@ require (
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/fzipp/gocyclo v0.6.0 // indirect
 	github.com/go-critic/go-critic v0.6.5 // indirect
-	github.com/go-logr/logr v1.2.0 // indirect
-	github.com/go-logr/zapr v1.2.0 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-toolsmith/astcast v1.0.0 // indirect
 	github.com/go-toolsmith/astcopy v1.0.2 // indirect
 	github.com/go-toolsmith/astequal v1.0.3 // indirect
@@ -182,17 +181,16 @@ require (
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	honnef.co/go/tools v0.3.3 // indirect
-	k8s.io/api v0.24.0 // indirect
-	k8s.io/apiextensions-apiserver v0.24.0 // indirect
-	k8s.io/apimachinery v0.24.0 // indirect
-	k8s.io/klog/v2 v2.60.1 // indirect
-	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9 // indirect
+	k8s.io/api v0.25.0 // indirect
+	k8s.io/apiextensions-apiserver v0.25.0 // indirect
+	k8s.io/apimachinery v0.25.0 // indirect
+	k8s.io/klog/v2 v2.70.1 // indirect
+	k8s.io/utils v0.0.0-20220728103510-ee6ede2d64ed // indirect
 	mvdan.cc/gofumpt v0.4.0 // indirect
 	mvdan.cc/interfacer v0.0.0-20180901003855-c20040233aed // indirect
 	mvdan.cc/lint v0.0.0-20170908181259-adc824a0674b // indirect
 	mvdan.cc/unparam v0.0.0-20220706161116-678bad134442 // indirect
-	sigs.k8s.io/controller-runtime/tools/setup-envtest v0.0.0-20221025140236-7d16aec4e84d // indirect
-	sigs.k8s.io/json v0.0.0-20211208200746-9f7c6b3444d2 // indirect
-	sigs.k8s.io/structured-merge-diff/v4 v4.2.1 // indirect
+	sigs.k8s.io/json v0.0.0-20220713155537-f223a00ba0e2 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
 	sigs.k8s.io/yaml v1.3.0 // indirect
 )
